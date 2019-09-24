905c6c3a95e8d08758c2cf16aa622d84264742fb
Jacob Mellichamp <40805336+Jacob-Mellichamp@users.noreply.github.com>
2019-09-23 23:43:46 -0400

    Update Grid.cpp

--- Grid.cpp
+++ Grid.cpp
@@ -1,3 +1,4 @@
+
 /*------------------------------------------------------------------------
  *                                 Grid.cpp
  * ------------------------------------------------------------------------
@@ -102,4 +103,254 @@ void grid::determineIfNode(int row, int col) {
 
 	}
 
+
+
+
+	//Determine what's in edgeDirection
+	bool lat = false;
+	bool vert = false;
+	if (totalEdges >= 2) {
+
+
+
+		for (int i = 0; i < edgeDirection.size(); i++) {
+			if (edgeDirection.at(i) == 'R' || edgeDirection.at(i) == 'L') {
+				lat = true;
+			}
+
+			if (edgeDirection.at(i) == 'U' || edgeDirection.at(i) == 'D') {
+				vert = true;
+			}
+		}
+
+	}
+
+
+
+
+	//Determine type of node
+	//start node
+	if (graph[row][col] == 's') {
+
+
+		sourceNode = new node(col, row);
+		//sourceNode->setPaths(edgeDirection);
+		pair<int, int> coordinates(col, row);
+		nodes[coordinates] = sourceNode;
+
+		//add edges to node
+		for (char c : edgeDirection) {
+			sourceNode->childMap[c];
+		}
+
+
+
+
+	}
+	//stop node
+	else if (graph[row][col] == 'f') {
+
+
+		stopNode = new node(col, row);
+		//stopNode->setPaths(edgeDirection);
+		pair<int, int> coordinates(col, row);
+		nodes[coordinates] = stopNode;
+
+		//add edges to node
+		for (char c : edgeDirection) {
+			stopNode->childMap[c];
+		}
+
+
+
+
+	}
+	//bomb node
+	else if (graph[row][col] == 'b') {
+		node* tmpNode;
+		tmpNode = new node(col, row);
+		
+
+		pair<int, int> coordinates(col, row);
+
+		//add edges to node
+		for (char c : edgeDirection) {
+			tmpNode->childMap[c];
+		}
+
+
+		nodes[coordinates] = tmpNode;
+
+		graph[row][col] = 'b';
+	}
+	//gold node
+	else if (graph[row][col] == 'g') {
+		node* tmpNode;
+		tmpNode = new node(col, row);
+
+
+		pair<int, int> coordinates(col, row);
+
+		//add edges to node
+		for (char c : edgeDirection) {
+			tmpNode->childMap[c];
+		}
+
+
+		nodes[coordinates] = tmpNode;
+
+		graph[row][col] = 'g';
+	}
+	//regular node
+	else if (lat && vert) {
+
+		node* tmpNode;
+		tmpNode = new node(col, row);
+		
+
+		pair<int, int> coordinates(col, row);
+
+		//add edges to node
+		for (char c : edgeDirection) {
+			tmpNode->childMap[c];
+		}
+
+
+		nodes[coordinates] = tmpNode;
+
+		graph[row][col] = 'n';
+
+
+
+	}
+}
+
+
+//Print the graph to console
+void grid::print() {
+	for (int r = 0; r < graph.size(); r++) {
+		cout << "|";
+		for (int c = 0; c < graph[0].size(); c++) {
+			cout << " " << graph[r][c] << "  |";
+		}
+		cout << endl;
+	}
+}
+
+
+//Connect the nodes to Childern.
+void grid::connectGraph() {
+
+	//find directions for all nodes.
+	auto iterator = nodes.begin();
+
+
+	while (iterator != nodes.end()) {
+
+
+		auto pathIterator = iterator->second->childMap.begin();
+
+
+		while (pathIterator != iterator->second->childMap.end()) {
+			visitNode((iterator->second), pathIterator->first);
+			pathIterator++;
+		}
+		iterator++;
+	}
+
+}
+
+
+//find next node
+void grid::visitNode(node* currNode, char direction) {
+
+	int x_search = currNode->getX();
+	int y_search = currNode->getY();
+
+
+	//If latter to decide which direction to look.
+	if (direction == 'U') {
+
+		//Conidition to know when to stop looking.
+		while (y_search > 0 && graph[y_search][x_search] != 'w') {
+			y_search--;
+
+
+			pair <int, int> currPosition(x_search, y_search);
+			//if the current position is a node, then add node as a child of parameterized node.
+			if (nodes.find(currPosition) != nodes.end()) {
+
+				currNode->childMap[direction] = nodes[currPosition];
+				return;
+			}
+		}
+	}
+
+	if (direction == 'D') {
+		//Conidition to know when to stop looking.
+		while (y_search < (graph.size() - 1) && graph[y_search][x_search] != 'w') {
+			y_search++;
+
+
+			pair <int, int> currPosition(x_search, y_search);
+			//if the current position is a node, then add node as a child of parameterized node.
+			if (nodes.find(currPosition) != nodes.end()) {
+
+				
+				currNode->childMap[direction] = nodes[currPosition];
+
+				return;
+			}
+		}
+
+	}
+
+	if (direction == 'R') {
+		//Conidition to know when to stop looking.
+		while (x_search < (graph[0].size() - 1) && graph[y_search][x_search] != 'w') {
+			x_search++;
+
+
+			pair <int, int> currPosition(x_search, y_search);
+			//if the current position is a node, then add node as a child of parameterized node.
+			if (nodes.find(currPosition) != nodes.end()) {
+
+				
+				currNode->childMap[direction] = nodes[currPosition];
+
+				return;
+			}
+		}
+	}
+
+	if (direction == 'L') {
+		//Conidition to know when to stop looking.
+		while (x_search > 0 && graph[y_search][x_search] != 'w') {
+			x_search--;
+
+
+			pair <int, int> currPosition(x_search, y_search);
+			//if the current position is a node, then add node as a child of parameterized node.
+			if (nodes.find(currPosition) != nodes.end()) {
+				//currNode->addChild(nodes[currPosition]);
+				currNode->childMap[direction] = nodes[currPosition];
+				return;
+			}
+		}
+
+	}
+}
+//Return the Manhatten Distance between two nodes.
+int grid::ManhatDist(node* parent, node* child) {
+	int x = abs(parent->getX() - child->getX());
+	int y = abs(parent->getY() - child->getY());
+
+	return x + y;
+
+}
+
+//Compute the total weight of a node
+int grid::calculateWeight(node* parent, int dist) {
+	return parent->getWeight() + dist;
+}
 
